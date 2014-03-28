4acbcf517d5a6f635d4dacb16560b866f8d0fe71
Thomas Craig <thomas@tjc.me.uk>
2014-03-28 09:55:03 +0000

    Added stats on main page

--- genomeportal/views.py
+++ genomeportal/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
-from genomeportal.annotations.models import Organism
+from genomeportal.annotations.models import Organism, SequenceType
 
 def index(request):
+    stats = SequenceType.objects.all()
     return render(request, 'index.jade', {
         'organisms': [o.common_name for o in Organism.objects.all()],
+        'stats': stats,
     })
