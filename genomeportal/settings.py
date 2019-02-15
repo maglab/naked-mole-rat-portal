64eaeac0c0e6dfe04925513ceef119976b0a1cdc
Zoya Farooq <zfarooq@Dans-MacBook-Pro.local>
2019-02-15 11:17:49 +0000

    change database name

--- genomeportal/settings.py
+++ genomeportal/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'nakedmolerat',                      # Or path to database file if using sqlite3.
+        'NAME': 'nmr',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
         'PASSWORD': 'postgres',
